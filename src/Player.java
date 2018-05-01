819348f0ee2a4c9b2841fb523ec1d8ecf29b7e8a
HaoFu1103 <38731399+HaoFu1103@users.noreply.github.com>
2018-04-30 23:01:25 -0700

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,22 @@
-
-public class Player {
-
+public class player {
+	private String name;
+	private int score;
+	
+	//constructs player
+	public player(String name, int score) {
+		this.name = name;
+		this.score = score;	
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public String toString() {
+		return "Player{" + "name =" + name + ", score ='" + score + '\'' + '}';
+	}
 }
