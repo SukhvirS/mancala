0462639cc9fb1defe8a7acebb335da686e33a01e
HaoFu1103 <38731399+HaoFu1103@users.noreply.github.com>
2018-04-30 23:09:35 -0700

    Update PitShape.java

--- src/PitShape.java
+++ src/PitShape.java
@@ -2,5 +2,8 @@ import java.awt.Color;
 import java.awt.geom.RectangularShape;
 public interface pitShape()
 {
+    public RectangularShape getPitShape();
+    public RectangularShape getMancalaShape();
     
+    public Color getPitColor();
 }
