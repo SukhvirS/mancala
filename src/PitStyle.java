18e7355524f5a01ddaee5a6b0e668167f725bf1f
HaoFu1103 <38731399+HaoFu1103@users.noreply.github.com>
2018-05-04 12:08:11 -0700

    Update PitStyle.java

--- src/PitStyle.java
+++ src/PitStyle.java
@@ -10,5 +10,11 @@ public interface PitStyle {
 	
 	// returns correct position of stone in pit
 	public RectangularShape getStoneShape(int num, boolean isMancala);
+	
+	public int getPitW();
+	public int getPitH();
+	
+	public int getMancalaW();
+	public int getMancalaH();
 	void paintComponent(Graphics g);
 }
