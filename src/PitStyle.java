44bfdca5fa098fd410e7a7a2891f16f4d0ed6891
HaoFu1103 <38731399+HaoFu1103@users.noreply.github.com>
2018-05-04 12:06:57 -0700

    Update PitStyle.java

--- src/PitStyle.java
+++ src/PitStyle.java
@@ -1,6 +1,14 @@
 import java.awt.Graphics;
 
 public interface PitStyle {
-
+	
+	//returns shape of pits
+	public RectangularShape getPitShape();
+	
+	//returns shape of mancalapits
+	public RectangularShape getMancalaPitShape();
+	
+	// returns correct position of stone in pit
+	public RectangularShape getStoneShape(int num, boolean isMancala);
 	void paintComponent(Graphics g);
 }
