d15ecd36ca9247d0c627d32a0b7fd68551347626
HaoFu1103 <38731399+HaoFu1103@users.noreply.github.com>
2018-05-04 11:13:31 -0700

    Update BoardColor.java

--- src/BoardColor.java
+++ src/BoardColor.java
@@ -1,5 +1,13 @@
 
 public interface BoardColor {
+	//returns color of the board;
+	public Color getBoardColor();
+	
+	//returns color of the stone
+	public Color getStoneColor();
+	
+	//returns color of the pit
+	public Color getpitColor();
 	
 	public void changeBoardColor();
 	
